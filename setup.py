0d600f99f3e6f12e07c28ac5bd61a2713055e507
Zach Dexter <zach.dexter@gmail.com>
2013-04-14 12:47:45 -0400

    PyPi setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='minisync',
+      version='0.01',
+      description='minsync Relational Object Synchronization',
+      author='Zach Dexter',
+      author_email='zach.dexter@gmail.com',
+      url='https://github.com/zdexter/minisync/',
+      packages=['minisync'],
+     )
